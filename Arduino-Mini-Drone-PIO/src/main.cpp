bb5b678fa247a526c420f40d48d7bcf76981d85f
Gaurav Bankar <gaurav210233@gmail.com>
2024-02-14 13:36:25 +0530

    cleared main

--- Arduino-Mini-Drone-PIO/src/main.cpp
+++ Arduino-Mini-Drone-PIO/src/main.cpp
@@ -1,33 +1,11 @@
 #include <Arduino.h>
-#include "custom_BMP280.h"
-#include "custom_MPU9250.h"
-
-
-CustomBMP280 bmp;
 
 void setup()
 {
   Serial.begin(115200);
-  bmp.init();
 }
 
 void loop()
 {
-  float altitude = bmp.getAltitude();
-  float pressure = bmp.getPressure();
-  float temperature = bmp.getTemperature();
-
-  // Print values or use them as needed
-  Serial.print("Altitude: ");
-  Serial.print(altitude);
-  Serial.print(" meters");
-
-  Serial.print("\tPressure: ");
-  Serial.print(pressure);
-  Serial.print(" Pa");
-
-  Serial.print("\tTemperature: ");
-  Serial.print(temperature);
-  Serial.println(" °C");
 
 }
